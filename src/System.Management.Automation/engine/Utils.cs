1685e7ab43d879cd999ccd39c803184d691edaca
Steve Lee <slee@microsoft.com>
2020-11-26 12:01:21 -0800

    fix codefactor issues

--- src/System.Management.Automation/engine/Utils.cs
+++ src/System.Management.Automation/engine/Utils.cs
@@ -1941,7 +1941,7 @@ namespace System.Management.Automation
                 PSStyle psstyle = (PSStyle)context.GetVariableValue(SpecialVariables.PSStyleVarPath);
                 if (psstyle is object)
                 {
-                    switch(formatStyle)
+                    switch (formatStyle)
                     {
                         case FormatStyle.Reset:
                             return psstyle.Reset;
